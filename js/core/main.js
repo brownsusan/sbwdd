c55e1a5acd63f2c4571a7e2866c69e5cb6744388
Susan Brown <brownsusan@fullsail.edu>
2014-05-02 21:03:05 -0400

    styles

--- js/core/main.js
+++ js/core/main.js
@@ -1,11 +1,19 @@
 $(document).on('mouseenter', '.project', function(event){
 	$(this).find('.project-details').css({
 		display: 'block'
-	})
+	});
+	
+	$(this).find('img').css({
+		'-webkit-filter': 'blur(3px) grayscale(50%)'
+	});
 });
 
 $(document).on('mouseleave', '.project', function(event){
 	$(this).find('.project-details').css({
 		display: 'none'
-	})
+	});
+	
+	$(this).find('img').css({
+		'-webkit-filter': 'none'
+	});
 });
