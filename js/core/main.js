1ac20eb7b9ff6dba23486ed6eb812e0fd6de086a
Susan Brown <brownsusan@fullsail.edu>
2014-05-07 00:53:33 -0400

    commit

--- js/core/main.js
+++ js/core/main.js
@@ -1,19 +1,27 @@
-$(document).on('mouseenter', '.project', function(event){
-	$(this).find('.project-details').css({
-		display: 'block'
-	});
-	
-	$(this).find('img').css({
-		'-webkit-filter': 'blur(3px) grayscale(50%)'
+$(document).ready(function() {
+	$('.process').find('h3').circleType({
+		'radius' : 384,
+		'fluid' : true
 	});
 });
 
-$(document).on('mouseleave', '.project', function(event){
+$(document).on('mouseenter', '.project', function(event) {
 	$(this).find('.project-details').css({
-		display: 'none'
+		display : 'block'
 	});
-	
+
 	$(this).find('img').css({
-		'-webkit-filter': 'none'
+		'-webkit-filter' : 'blur(3px) grayscale(50%)'
 	});
 });
+
+$(document).on('mouseleave', '.project', function(event) {
+	$(this).find('.project-details').css({
+		display : 'none'
+	});
+
+	$(this).find('img').css({
+		'-webkit-filter' : 'none'
+	});
+});
+
