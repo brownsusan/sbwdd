a3f478ae83ad1ddefbdaca5471bd260828dedfbc
Susan Brown <brownsusan@fullsail.edu>
2014-05-08 01:54:52 -0400

    fade in - figuring out heights

--- js/core/main.js
+++ js/core/main.js
@@ -1,22 +1,33 @@
 $(document).on('mouseenter', '.project', function(event) {
-	$(this).find('.project-details-container').css({
-		display : 'block'
+	
+
+	$(this).find('.project-details-container').fadeIn(400, function() {
+		// Animation complete
 	});
-	$(this).find('.project-details-content').css({
-		display : 'block'
+	$(this).find('.project-details-content').fadeIn(400, function() {
+		// Animation complete
 	});
 
 	$(this).find('img').css({
-		'-webkit-filter' : 'blur(10px) grayscale(50%)'
+		'-webkit-filter' : 'grayscale(100%)'
 	});
 });
 
 $(document).on('mouseleave', '.project', function(event) {
-	$(this).find('.project-details-container').css({
-		display : 'none'
+	// $(this).find('.project-details-container').css({
+		// display : 'none'
+	// });
+// 
+	// $(this).find('.project-details-content').css({
+		// display : 'none'
+	// });
+	
+	$(this).find('.project-details-container').fadeOut("fast", function() {
+		// Animation complete
 	});
-	$(this).find('.project-details-content').css({
-		display : 'block'
+	
+	$(this).find('.project-details-content').fadeOut("fast", function() {
+		// Animation complete
 	});
 
 	$(this).find('img').css({
