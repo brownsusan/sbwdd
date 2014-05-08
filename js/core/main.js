ba8ac836f09f79f542273423a86caaee8f59c1bc
Susan Brown <brownsusan@fullsail.edu>
2014-05-07 22:09:13 -0400

    styled resume - working on portfolio page

--- js/core/main.js
+++ js/core/main.js
@@ -1,20 +1,27 @@
-$(document).ready(function() {
-	$('.process').find('h3').arctext({radius: 100, dir : -1});
-});
+// $('.process').find('h3').arctext({
+	// radius : 100,
+	// dir : -1
+// });
 
 $(document).on('mouseenter', '.project', function(event) {
-	$(this).find('.project-details').css({
+	$(this).find('.project-details-container').css({
+		display : 'block'
+	});
+	$(this).find('.project-details-content').css({
 		display : 'block'
 	});
 
 	$(this).find('img').css({
-		'-webkit-filter' : 'blur(3px) grayscale(50%)'
+		'-webkit-filter' : 'blur(20px) grayscale(50%)'
 	});
 });
 
 $(document).on('mouseleave', '.project', function(event) {
-	$(this).find('.project-details').css({
+	$(this).find('.project-details-container').css({
 		display : 'none'
+	});
+	$(this).find('.project-details-content').css({
+		display : 'block'
 	});
 
 	$(this).find('img').css({
