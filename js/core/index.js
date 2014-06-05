f32d94819cd77cbdf357c6d2f66fee3b50ad7534
Susan Brown <brownsusan@fullsail.edu>
2014-06-05 02:41:21 -0400

    start redesign

--- js/core/index.js
+++ js/core/index.js
@@ -1,4 +1,4 @@
-$('.process').find('h3').arctext({
-	radius : 100,
-	dir : -1
-});
+// $('.process').find('h3').arctext({
+	// radius : 100,
+	// dir : -1
+// });
--- js/core/main.js
+++ js/core/main.js
@@ -1,37 +1,15 @@
-$(document).on('mouseenter', '.project', function(event) {
-	
-
-	$(this).find('.project-details-container').fadeIn(400, function() {
-		// Animation complete
-	});
-	$(this).find('.project-details-content').fadeIn(400, function() {
-		// Animation complete
-	});
-
-	$(this).find('img').css({
-		'-webkit-filter' : 'grayscale(100%)'
-	});
+$(document).ready(function(){
+	resizeImage();
 });
 
-$(document).on('mouseleave', '.project', function(event) {
-	// $(this).find('.project-details-container').css({
-		// display : 'none'
-	// });
-// 
-	// $(this).find('.project-details-content').css({
-		// display : 'none'
-	// });
-	
-	$(this).find('.project-details-container').fadeOut("fast", function() {
-		// Animation complete
-	});
-	
-	$(this).find('.project-details-content').fadeOut("fast", function() {
-		// Animation complete
-	});
-
-	$(this).find('img').css({
-		'-webkit-filter' : 'none'
-	});
+$(window).resize(function() {
+	resizeImage();
 });
 
+var resizeImage = function(){
+	//Need to reset css here when the window is scaled back up past the media queries
+	var projectImageWidth = $('.project img').width();
+	$('.project img').height(projectImageWidth);
+	$('.project-details-container').width(projectImageWidth);
+	$('.project-details-container').height(projectImageWidth);
+};
